3e5dd26ef05db0a81314995f0af237bb5b552ee3
christikaes <christikaes@gmail.com>
2017-07-20 11:32:30 -0400

    Part2-1

---
+++ database.js
@@ -0,0 +1,11 @@
+var getNewBox = function() {
+    return {
+      name: "My Musical Box",
+      data: new Array(2).fill(new Array(4).fill(false)),
+    }
+  }
+
+addBox = function() {
+    var box = getNewBox();
+    // TODO: Add a new box 
+}
