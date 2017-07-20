284425e687e0acc1ecb90856bffebabdaaa71f8f
christikaes <christikaes@gmail.com>
2017-07-20 11:21:24 -0400

    Part1-1

---
+++ auth.js
@@ -0,0 +1,17 @@
+login = function() {
+    document.getElementById("error").innerText = "";
+    var email = document.getElementById("login-email").value;
+    var password = document.getElementById("login-password").value;
+    // TODO: login with this email and password
+};
+
+register = function() {
+    document.getElementById("error").innerText = "";
+    var email = document.getElementById("register-email").value;
+    var password = document.getElementById("register-password").value;
+    // TODO: register with this email and password
+};
+
+logout = function() {
+    // TODO: logout the current user
+};
